dd24c7e64aaae2b139a8e9f7e7a928d918ab436a
Mrbysco <Mrbysco@users.noreply.github.com>
2021-12-01 02:41:04 +0100

    Initial port to 1.18 [push]

--- src/main/java/com/mrbysco/padoru/init/ModRegistry.java
+++ src/main/java/com/mrbysco/padoru/init/ModRegistry.java
@@ -10,9 +10,9 @@ import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeSpawnEggItem;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Padoru.MOD_ID);
