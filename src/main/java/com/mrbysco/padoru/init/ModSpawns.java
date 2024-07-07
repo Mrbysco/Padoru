50af0b895fb761eefbfb230e073b5c099ffe551b
Mrbysco <Mrbysco@users.noreply.github.com>
2024-07-07 22:09:52 +0200

    Update NeoForge supprot

--- src/main/java/com/mrbysco/padoru/init/ModSpawns.java
+++ src/main/java/com/mrbysco/padoru/init/ModSpawns.java
@@ -4,12 +4,12 @@ import com.mrbysco.padoru.entity.Padoru;
 import net.minecraft.world.entity.SpawnPlacementTypes;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
-import net.neoforged.neoforge.event.entity.SpawnPlacementRegisterEvent;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 
 public class ModSpawns {
 
-	public static void registerEntityAttributes(SpawnPlacementRegisterEvent event) {
-		event.register(ModRegistry.PADORU.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Padoru::checkMobSpawnRules, SpawnPlacementRegisterEvent.Operation.AND);
+	public static void registerEntityAttributes(RegisterSpawnPlacementsEvent event) {
+		event.register(ModRegistry.PADORU.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Padoru::checkMobSpawnRules, RegisterSpawnPlacementsEvent.Operation.AND);
 	}
 
 	public static void registerSpawnPlacements(EntityAttributeCreationEvent event) {
